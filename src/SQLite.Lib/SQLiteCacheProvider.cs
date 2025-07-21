479b18c9db5042b7454b526893518f39c56d9b37
Xiaodong Li <xiaodoli@microsoft.com>
2025-07-21 15:47:45 -0700

    fix cache provider

---
+++ src/SQLite.Lib/SQLiteCacheProvider.cs
@@ -0,0 +1,13 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SQLiteCacheProvider.cs" company="Microsoft Corp.">
+//     Copyright (c) Microsoft Corp. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SQLite.Lib
+{
+    public class SQLiteCacheProvider
+    {
+        
+    }
+}
